df3a872d4d4b3d8d87e0a62fa6df68a2cc0b97d4
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-08-15 21:02:03 -0500

    Basic implementation of useEffect

---
+++ .history/src/components/UseEffectExample_20210815210136.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export default function UseEffectExample() {
+    const [size, setSize] = useState(getSize())
+
+    function getSize() {
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+    }
+
+    const handleWindowResize = () => setSize(getSize());
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+        return () =>window.removeEventListener('resize', handleWindowResize);
+    });
+    return (
+        <p>Width: {size.width}, Height: {size.height}</p>
+    )
+}
