538fb5e7f424b73136d6c012d271a5d58baa9e86
Bharath Seshadri <Bharath.Seshadri@charter.com>
2021-09-23 14:31:59 +0300

    Implemented UserPicker with useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import UserStorage from './components/UserStorage';
+import UserPicker from './components/UserPicker';
 
 function App() {
   return (
-    <div className="App">
-       <UserStorage/>
-    </div>
+    <>
+      <div className="App">
+        <UserStorage/>
+      </div>
+      <div className="user-picker">
+        <UserPicker/>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/UserPicker.js
@@ -0,0 +1,24 @@
+import {FaSpinner} from "react-icons/fa";
+import { useEffect, useState } from 'react';
+
+export default function UserPicker() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('http://localhost:3001/users');
+            const data = await response.json();
+            setUsers(data);
+        }
+        setTimeout(fetchData, 5000);
+    }, []);
+
+    if (users.length === 0) {
+        return <FaSpinner className='icon-loading'/>
+    }
+
+    return (
+        <select>
+            {users.map(user => <option key={user.id}>{user.name}</option>)}
+        </select>
+    )
+}
